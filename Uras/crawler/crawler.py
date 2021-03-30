346d1977963fc0bd3eab589845472165463f9dee
Mickey Shaughnessy <mickey.shaughnessy@vdx.tv>
2021-03-29 21:27:49 -0700

    add crawler

---
+++ Uras/crawler/crawler.py
@@ -0,0 +1,18 @@
+import time
+import redis
+import requests
+
+
+def get_card_data():
+    # gets a card from scryfall
+    # inserts it into the db
+
+def crawl():
+    time.sleep(1)
+    get_card_data()
+
+
+if __name__ == "__main__":
+
+    while True:
+        crawl()
